22a02d5b0febda7b7bf787ff6632a715d0a77468
Haozhe-H <haozhehuang@outlook.com>
2023-05-01 16:32:06 -0400

    minor change navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Styles.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Styles.css";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About Me</Link>
-        </li>
-        <li>
-          <Link to="/work">Work</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact Me</Link>
-        </li>
-        <li>
-          <a href="#">Resume</a>
-        </li>
-      </ul>
-    </nav>
+    <header class="header">
+      <h1>Haozhe Huang</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Me</Link>
+          </li>
+          <li>
+            <Link to="/work">Work</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Me</Link>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
